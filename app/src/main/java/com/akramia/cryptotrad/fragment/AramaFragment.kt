77c481936c6b871ed5b58eacae4aab2fe485c199
AkramAkbarov <153746309+AkramAkbarov@users.noreply.github.com>
2024-03-01 13:47:27 +0400

    crypto

--- app/src/main/java/com/akramia/cryptotrad/fragment/AramaFragment.kt
+++ app/src/main/java/com/akramia/cryptotrad/fragment/AramaFragment.kt
@@ -1,14 +1,23 @@
 package com.akramia.cryptotrad.fragment
 
 import android.os.Bundle
+import android.text.style.LineHeightSpan.WithDensity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import com.akramia.cryptotrad.R
 import com.akramia.cryptotrad.adapter.MaeketAdapter
+import com.akramia.cryptotrad.apis.ApiInterface
+import com.akramia.cryptotrad.apis.ApiUtilities
 import com.akramia.cryptotrad.databinding.FragmentAramaBinding
 import com.nexis.cryptoapp.models.CryptoCurrency
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.create
 
 class AramaFragment : Fragment() {
 
@@ -23,6 +32,19 @@ class AramaFragment : Fragment() {
         list = listOf()
         adapter = MaeketAdapter(requireContext(),list,"market")
         binding.SearchRecyler.adapter =adapter
-        return inflater.inflate(R.layout.fragment_arama, container, false)
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            val res = ApiUtilities.getInstance().create(ApiInterface::class.java).getMarketData()
+            if (res.body()!=null){
+                withContext(Dispatchers.Main){
+
+                    list =res.body()!!.data.cryptoCurrencyList
+                    adapter.upDateData(list)
+                    binding.spinKitView.visibility=GONE
+                }
+
+            }
+        }
+        return binding.root
     }
 }
